41f962cf106488f4213c92b55954157b5f682be7
Mohammad Shojaei <shojaeireal@gmail.com>
2023-10-02 22:19:20 +0330

    Update coin_flipper.py

--- Coin Flipper/coin_flipper.py
+++ Coin Flipper/coin_flipper.py
@@ -1,31 +1,31 @@
 import random
 import flet as ft
 
-x = "head"
-y = "tail"
+x = "Head"
+y = "Tail"
 coin = random.choice ([x,y])
 
 def main (page : ft.Page) :
-    page.title = "Coin Flipper"
+    page.tite = "Coin Flipper"
     page.add(ft.Text(value=coin))
     img1 = ft.Image(
-        src=f"E:\CS\haed.jpg",
-        width=100,
-        height=100,
+        src= r"E:\python apps\flet\flipcoin\haed.jpg" ,
+        width=200,
+        height=200,
         fit=ft.ImageFit.CONTAIN,
     )
     img2 = ft.Image(
-        src=f"E:\CS\htail.jpg",
-        width=100,
-        height=100,
+        src=r"E:\python apps\flet\flipcoin\htail.jpg",
+        width=200,
+        height=200,
         fit=ft.ImageFit.CONTAIN,
     )
     if coin == x :
         page.add(img1)
     else:
         page.add(img2)
-    page.window_width = 200        # window's width is 200 px
-    page.window_height = 200       # window's height is 200 px
+    page.window_width = 350        # window's width is 200 px
+    page.window_height = 350       # window's height is 200 px
     page.window_resizable = False  # window is not resizable
     
     page.update()
